c95bd9db1520deb4c16040e6bf4b4f42737c1eb0
Kianté Fernandez <61021880+kiante-fernandez@users.noreply.github.com>
2024-06-03 17:30:57 -0700

    refactor the to be final estimation file for HSSM models

--- src/hssm_reproduce_choice_history_ddm.py
+++ src/hssm_reproduce_choice_history_ddm.py
@@ -10,7 +10,7 @@
 # 2024/01/08      Kianté Fernandez<kiantefernan@gmail.com>   added util functions for workflow
 # 2024/07/03      Kianté Fernandez<kiantefernan@gmail.com>   added util functions for workflow
 
-# %%
+# %% load lib
 import sys, os, time
 import arviz as az
 import numpy as np
@@ -20,11 +20,8 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import re
 
-from src.utils_hssm_modelspec import make_model # specifically for hssm models
-#from utils_hssm import run_model, saveInferenceData
-from utils_hssm import run_model, dic, aggregate_model_comparisons, reattach
-#import utils_hssm
-#from utils_hssm import saveInferenceData
+from src.utils_hssm_modelspec import make_model 
+from utils_hssm import run_model, reattach
 
 # %% load data
 script_dir = os.path.dirname(os.path.realpath(__file__))
@@ -32,16 +29,14 @@ data_file_path = os.path.join(script_dir, '..', '..', '2023_choicehistory_HSSM',
 elife_data = pd.read_csv(os.path.join(data_file_path, 'visual_motion_2afc_fd.csv'))
 
 elife_data['signed_contrast'] = elife_data['coherence'] * elife_data['stimulus']
-
 elife_data['response'] = elife_data['response'].replace({0: -1, 1: 1})
-
 # # add some more history measures
 elife_data['stimrepeat'] = np.where(elife_data.stimulus == elife_data.prevstim, 1, 0)
 elife_data['repeat'] = np.where(elife_data.response == elife_data.prevresp, 1, 0)
 
 elife_data['participant_id'] = elife_data['subj_idx']
-excluded_participants = [11, 19, 20, 22, 26, 27, 28] #whose subject level fits did not converge
 
+excluded_participants = [11, 19, 20, 22, 26, 27, 28] #whose subject level fits did not converge
 elife_data = elife_data[~elife_data['participant_id'].isin(excluded_participants)]
 
 def get_prep(data):
@@ -54,8 +49,7 @@ prep = pd.DataFrame(get_prep(elife_data))
 # num_subjects = 5
 # # # Group the data by subject, sample the desired number of subjects, and get all rows for these subjects
 # subsample = elife_data[elife_data['subj_idx'].isin(elife_data.groupby('subj_idx').size().sample(num_subjects).index)]
-
-# %%
+# %% specify HSSM models
 # Define models
 model_names = [
     "ddm_nohist",
@@ -69,8 +63,7 @@ model_names = [
 # ]
 
 ddm_models = {name: make_model(elife_data, name) for name in model_names}
-
-# %% parameter estimation
+# %% run parameter estimation
 # Parameters for sampling
 sampling_params = {
     "sampler": "nuts_numpyro",
@@ -83,12 +76,17 @@ sampling_params = {
 
 # Sample from the posterior for each model
 model_run_results = {name: run_model(elife_data, name, script_dir, **sampling_params) for name in model_names}
-# %% here we would use Z's plotting code
+# %% plotting here we would use Z's plotting code
 # Base directory for model files and figures
 results_dir = os.path.join(script_dir, 'results', 'figures')
 
-# %% plot it
-# %% plot conditional bias function
+
+
+
+
+# %% plot other things not yet included in Z's code: conditional bias function
+
+## Conditional bia function 
 def conditional_history_plot(df, quantiles):
     import seaborn as sns
 
@@ -126,20 +124,20 @@ quantiles = [0, 0.1, 0.3, 0.5, 0.7, 0.9]
 fig = conditional_history_plot(elife_data, quantiles)
 plt.show()
 
-#%% Posterior predictives on psychometric/chronometric functions
-
-# reattch to the corresponding HSSM object
-no_hist = reattach("/Users/kiante/Documents/2023_choicehistory_HSSM/data/ddm_nohist_model.nc", "ddm_nohist", elife_data)
-no_hist.traces
-no_hist.sample_posterior_predictive(data=elife_data, n_samples = 10, include_group_specific = True)
-# #note once model is rerun you can sample
-hssm.hssm.plotting.plot_posterior_predictive(model= no_hist)
-hssm.hssm.plotting.plot_quantile_probability(model= no_hist, cond="stimulus")
-
-plotting_df = hssm.hssm.plotting.posterior_predictive._get_plotting_df(idata=no_hist.traces, data=elife_data,extra_dims=["subj_idx","prevresp"])
-
-#compute choice prevresp on the simulated data from PPC
-
-# %% # Quantile probability plots 
+## Quantile probability plot 
 fig = hssm.plotting.quantile_probability(ddm_models['ddm_nohist'], cond="signed_contrast")
 fig.set_ylim(0, 3);
+plt.show()
+
+#%% Posterior predictives checks on models
+
+# reattch to the corresponding HSSM object
+prevresp_zv = reattach(os.path.join(data_file_path, 'ddm_prevresp_zv.nc'), "ddm_prevresp_zv", elife_data)
+prevresp_zv.traces
+prevresp_zv.sample_posterior_predictive(data=elife_data, n_samples = 10, include_group_specific = True)
+# #note once model is rerun you can sample
+hssm.hssm.plotting.plot_posterior_predictive(model= prevresp_zv)
+
+plotting_df = hssm.hssm.plotting.posterior_predictive._get_plotting_df(idata=prevresp_zv.traces, data=elife_data,extra_dims=["subj_idx","prevresp"])
+
+#compute choice prevresp on the simulated data from PPC
